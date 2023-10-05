9904fa4d83cfa39d7f34bab38e2ed8f4b3a3e46e
j0nk1ns <97742736+j0nk1ns@users.noreply.github.com>
2023-10-05 12:44:31 -0400

    Wall

---
+++ Assets/MINE/Scripts/PlayerController2.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public float moveSpeed;
+	public bool rushing = false;
+	private float speedMod = 0;
+
+	float timeLeft = 2f;
+
+	private Rigidbody2D rigidbody2d;
+    private Rigidbody2D myRigidbody;
+
+	public GameObject bubbles;
+
+    float horizontal; 
+    float vertical;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        resetBoostTime ();
+		controllerManager (); 
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 20.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 20.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+
+   void controllerManager ()
+    {
+	
+		if(Input.GetButtonDown("Jump") && !rushing )
+        {
+			rushing = true;
+			speedMod = 2;
+			Instantiate (bubbles, gameObject.transform.position, gameObject.transform.rotation);
+			movePlayer ();
+		}	
+	}
+
+	void movePlayer()
+	{
+		if (transform.localScale.x == 1)  
+        {
+			myRigidbody.velocity = new Vector3 (moveSpeed + speedMod, myRigidbody.velocity.y, 0f);	
+		} 
+
+        else 
+		{
+			myRigidbody.velocity = new Vector3 (- (moveSpeed + speedMod), myRigidbody.velocity.y, 0f);
+		}	
+	}
+
+	void resetBoostTime()
+	{
+		if (timeLeft <= 0) 
+        {
+			timeLeft = 10f;
+			rushing = false;
+			speedMod = 0;
+		}
+       else if(rushing) 
+       {
+	       timeLeft -= Time.deltaTime;
+        }
+    }
+}			
+	
+
---
+++ Assets/MINE/Scripts/TheCameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheCameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, -10);	
+	}
+}

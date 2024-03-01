bca4f2b6607fcce5fecdcd4d8104c171bdd2e78a
j0nk1ns <97742736+j0nk1ns@users.noreply.github.com>
2024-03-01 14:07:00 -0500

    o

---
+++ Assets/OURS/Scenes/New Folder/GameStart.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameStart : MonoBehaviour
+{
+    public Button startGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+    }
+   public void StartGame()
+    {
+        SceneManager.LoadScene("Game");   
+    }
+}
